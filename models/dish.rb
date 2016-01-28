70089fe8165a0304b0175384035e5c15345796e4
Luke Todd <lukejamestodd1@gmail.com>
2016-01-28 15:38:02 +1100

    hdhdhd

--- models/dish.rb
+++ models/dish.rb
@@ -8,8 +8,8 @@ CarrierWave.configure do |config|
     :provider              => 'AWS',
     :aws_access_key_id     => ENV['S3_ACCESS_KEY_ID'],
     :aws_secret_access_key => ENV['S3_SECRET_ACCESS_KEY'],
-    :region                => 'ap-southeast-2',
-    :host   => 's3-ap-southeast-2.amazonaws.com'
+    :region                => 'ap-southeast-2'
+    # :host   => 's3-ap-southeast-2.amazonaws.com'
   }
     config.storage = :fog
   config.fog_directory    = 'gruelintentions'
